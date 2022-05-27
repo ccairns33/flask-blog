3991f889f96df7859d77e815ab4acb7f9c74118d
Carla Cairns <atl-carlac@corp.expeditors.com>
2022-05-27 15:36:39 -0500

    success and failure messages added for login screen. Tested with dummy data

--- app.py
+++ app.py
@@ -35,13 +35,18 @@ def register():
         flash(f"Account created for {form.username.data}!","success")
         return redirect(url_for("home"))
         # home is the name of the function for that route, not the route itself
-    else:
-        flash("")
+    
     return render_template("register.html", title="Register", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username or password.", "danger")
     return render_template("login.html", title="Login", form=form)
 
 
